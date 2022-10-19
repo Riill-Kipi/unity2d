a7d49c89c9b905e786c6bda72c13f77e95703d39
Riill-Kipi <69919163+Riill-Kipi@users.noreply.github.com>
2022-10-19 13:54:24 +0500

    +jupm
    23

--- YP/Assets/Script/playcontrol.cs
+++ YP/Assets/Script/playcontrol.cs
@@ -6,20 +6,84 @@ public class playcontrol : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float HorizontalMove = 0f;
-    public float speed = 1f;
+    private bool FacingRight = true;
+
+    [Header("Player Movement Setting")]
+    [Range(0f, 10f)] public float speed = 0.5f;
+    [Range(0f, 15f)] public float jumpForce = 8f;
+
+    [Space]
+    [Header("Ground Checker Setting")]
+    public bool isGrounded = false;
+    [Range(-5f, 5f)] public float checkGroundOffsetY = -1.8f;
+    [Range(0f, 5f)] public float checkGroundRadius = 0.3f;
+
+    [Header("Playr Animation Setting")]
+    public Animator animator;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+
         HorizontalMove = Input.GetAxisRaw("Horizontal")* speed;
+
+        animator.SetFloat("HorizontalMove", Mathf.Abs(HorizontalMove));
+
+        if(isGrounded == false)
+        {
+            animator.SetBool("Jumping", true);
+        }
+        else
+        {
+            animator.SetBool("Jumping", false);
+        }
+
+
+        if (HorizontalMove < 0 && FacingRight)
+        {
+            Flip();
+        }
+        else if (HorizontalMove > 0 && !FacingRight)
+        {
+            Flip();
+        }
     }
 
     private void FixedUpdate()
     {
-        
+        Vector2 targetVelocity = new Vector2(HorizontalMove * 10f, rb.velocity.y);
+        rb.velocity = targetVelocity;
+        CheckGround();
+    }
+
+    private void Flip()
+    {
+        FacingRight = !FacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll
+            (new Vector2(transform.position.x, transform.position.y + checkGroundOffsetY), checkGroundRadius);
+
+        if(colliders.Length < 1)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 }
